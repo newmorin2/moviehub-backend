ee068ee96d282a7786cedf4b60006b1c03032105
Ray Wekesa <ray.wekesa@student.moringaschool.com>
2026-06-16 13:57:25 +0300

    Fixed the fetch errors

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes.movie_routes import router as movie_router
+from app.api.endpoints import router as api_router
 from app.core.auth import initialize_firebase
 from contextlib import asynccontextmanager
-from app.api.endpoints import router as api_router
-
-app = FastAPI(title="MovieHub API", version="1.0.0")
-
 
 origins = [
     "http://localhost:5173",
     "http://localhost:3000",
 ]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    initialize_firebase()
+    yield
+
+app = FastAPI(title="MovieHub API", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,24 +26,8 @@ app.add_middleware(
 )
 
 app.include_router(movie_router)
+app.include_router(api_router)
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the MovieHub API backend!"}
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    initialize_firebase()
-    yield
-
-app = FastAPI(lifespan=lifespan)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-app.include_router(api_router)
